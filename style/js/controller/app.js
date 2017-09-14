c21658a20cdbb62c0d4b0fbeae77c286a3700666
Mathew Maione <mathew@kavada.com>
2017-09-14 15:49:03 -0700

    Updated the way locations are retrieved, added a json file to hold data for all locations and possible loot. Updated design changes and moving xur.

--- style/js/controller/app.js
+++ style/js/controller/app.js
@@ -3,14 +3,17 @@ class App {
 	init() {
 		this.appInterval();
 		this.buildTemplate();
-		this.buildTimer();
 		this.buildLocations();
+		this.buildTimer();
+		this.resizeTemplate();
+		this.moveXur();
 	}
 
 	appInterval() {
 		var app = new App;
 	    setInterval(function() {
 	    	app.buildTimer();
+	    	app.moveXur();
 	    }, 1e3);
 	}
 
@@ -18,8 +21,10 @@ class App {
 		var template, content;
 		template = jQuery('.xur-app');
 		content = 	`<div class="xur-template">
-						<div class="xur-banner" id="top-image">
-							<div class="xur-banner-title"></div>
+						<div class="xur-banner">
+							<div class="xur-banner-image xur-25">
+								<div class="xur-banner-title"></div>
+							</div>
 						</div>
 						<div class="xur-timer">Timer</div>
 						<div class="xur-locations xur-wrapper"></div>
@@ -28,6 +33,16 @@ class App {
 	}
 
 	buildTimer() {
+		var data = JSON.parse(localStorage.locations);
+		var time;
+		for (var i = 0; i < Object.keys(data).length; i++) {
+			launch = moment(data[i].time);
+	        today = moment().format();
+	        seconds = (launch.diff(today, 'seconds'));
+	        if(seconds > 0 && data[i].available === true) {
+	        	time = data[i].time;
+	        }
+		}
 		var timer, content, launch, today, days, day, hours, hour, minutes, minute, nm, seconds, second;
 		var t = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"),
             e = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
@@ -43,10 +58,12 @@ class App {
             u = n.getSeconds(),
             l = "AM";
         h >= 12 && (l = "PM"), h > 12 && (h -= 12), 0 == h && (h = 12), 9 >= c && (c = "0" + c), 9 >= u && (u = "0" + u), $(".mhn-ui-date-time .mhn-ui-day").text(t[a]), $(".mhn-ui-date-time .mhn-ui-date").text(e[o] + " " + s + ", " + i), $(".mhn-ui-date-time .mhn-ui-time").text(h + ":" + c + ":" + u + " " + l), $(".mhn-ui-app-time").text(h + ":" + c + ":" + u + " " + l)
-        launch = moment('2017-09-14T23:59:59');
+        launch = moment(time);
         today = moment().format();
-        days = (launch.diff(today, 'days'));
-        hours = (launch.diff(today, 'hours')) - 24;
+        days = launch.diff(today,'days');
+        hours = launch.diff(today,'hours');
+        if(hours<24){hours=hours;}
+        else {hours=hours-24;}
         minutes = (launch.diff(today, 'minutes'));
         seconds = (launch.diff(today, 'seconds'));
 		nm = Math.round( ((launch.diff(today, 'hours') - minutes/60) * 60), 2);
@@ -76,10 +93,87 @@ class App {
 		timer.html(content);
 	}
 
+	timerTemplate() {
+
+	}
+
 	buildLocations() {
-		for (var i = 1; i <= 9; i++) {
-			jQuery('.xur-locations').append('<div class="xur-location"><div class="xur-location-block">'+i+'</div></div>');
+		var storage = new Storage(), app = new App();
+		jQuery.getJSON("/data/locations.json", function(json) {
+            if(Object.keys(json).length < 1) {
+                console.log('empty');
+            }
+            else {
+                storage.locations(json);
+                app.loadLocations(json);
+            }
+        });
+	}
+
+	loadLocations(json) {
+		for (var i = 0; i < Object.keys(json).length; i++) {
+			var data, content;
+			data = json[i];
+			var loot = (data.available ? this.loadLoot(data,i) : '<div class="xur-location-unknown">unknown</div>');
+			var available = (data.available ? 'available' : 'not-available');
+			content = 	`<div class="xur-location">
+							<div class="xur-location-block `+available+`">
+								<div class="xur-location-loot xur-wrapper">`+loot+`</div>
+								<div class="xur-location-time">`+moment(data.time).format("MMM Do YYYY")+`</div>
+							</div>
+						</div>`;
+			jQuery('.xur-locations').append(content);
+			this.buildCarousel();
 		}
+	}
+
+	loadLoot(data,num) {
+		var content;
+		content = 	`<div class="loot-carousel owl-carousel owl-theme">
+					    <div class="item">
+					    	<img src="http://via.placeholder.com/1920x1080" width="100%">
+					    </div>
+					    <div class="item">
+					    	<img src="http://via.placeholder.com/1920x1080" width="100%">
+					    </div>
+					    <div class="item">
+					    	<img src="http://via.placeholder.com/1920x1080" width="100%">
+					    </div>
+					    <div class="item">
+					    	<img src="http://via.placeholder.com/1920x1080" width="100%">
+					    </div>
+					    <div class="item">
+					    	<img src="http://via.placeholder.com/1920x1080" width="100%">
+					    </div>
+					</div>`;
+		return content;
+	}
+
+	buildCarousel() {
+		jQuery('.loot-carousel').owlCarousel({
+		    loop: true,
+		    margin: 0,
+		    nav: false,
+		    responsive:{
+		        0:{ items: 1 }
+		    }
+		});
+	}
+
+	resizeTemplate() {
+		jQuery(window).resize(function() {
+			var loot, lootHeight;
+			loot = jQuery('.loot-carousel');
+			lootHeight = loot.height();
+			jQuery('.xur-location-block.not-available').css({'min-height':lootHeight+'px'});
+		});
+	}
+
+	moveXur() {
+		var randX, randY;
+		randX = Math.floor(Math.random() * -25) + 1;
+		randY = Math.floor(Math.random() * -25) + 1;
+		jQuery('.xur-banner-image').css({'background-position-x':'calc(50% - '+randX+'px)','background-position-y':'calc(50% - '+randY+'px)'});
 	}
 
 }
--- style/js/controller/storage.js
+++ style/js/controller/storage.js
@@ -1,7 +1,11 @@
 class Storage {
 
 	init() {
-		
+
+	}
+
+	locations(data) {
+		localStorage.setItem('locations', JSON.stringify(data));
 	}
 
 }
